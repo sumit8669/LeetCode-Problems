ca6acca1fead1651b836b9ca0679d47af14ad9eb
sumit Rana <sumit2628r@gmail.com>
2023-02-26 00:45:10 +0530

    single number updated

--- Data Structure/src/data_structure_II/SingleNumber.java
+++ Data Structure/src/data_structure_II/SingleNumber.java
@@ -1,5 +1,18 @@
 package data_structure_II;
 
+import java.util.HashMap;
+
 public class SingleNumber {
+
+    public int singleNumber(int[] nums) {
+        HashMap <Integer , Integer> hm  = new HashMap<>();
+
+        for (int i = 0; i <nums.length ; i++) {
+              hm.put(i,nums[i]);
+        }
+
+
+        return 0;
+    }
 
 }
