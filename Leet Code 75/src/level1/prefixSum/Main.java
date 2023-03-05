c3d24dbeb44502836a9e659fbe48c60020e4aa67
sumit Rana <sumit2628r@gmail.com>
2023-03-05 23:28:58 +0530

    leetcode 75 main updated

---
+++ Leet Code 75/src/level1/prefixSum/Main.java
@@ -0,0 +1,12 @@
+package level1.prefixSum;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Find_Pivot_Index fp = new Find_Pivot_Index();
+        int [] num = {1,2,3,4,5,6,7,8,9,10};
+
+        System.out.println(fp.pivotIndex(num));;
+    }
+}
