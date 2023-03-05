65de7877350018d5be81846784c98017e8854fdc
sumit Rana <sumit2628r@gmail.com>
2023-03-05 23:28:40 +0530

    leetcode 75 fp updated

---
+++ Leet Code 75/src/level1/prefixSum/Find_Pivot_Index.java
@@ -0,0 +1,12 @@
+package level1.prefixSum;
+
+public class Find_Pivot_Index {
+    public int pivotIndex(int[] nums) {
+        int totalSum =0;
+        for(int i=0; i < nums.length; i++){
+                totalSum +=nums[i];
+        }
+
+        return -1;
+    }
+}
