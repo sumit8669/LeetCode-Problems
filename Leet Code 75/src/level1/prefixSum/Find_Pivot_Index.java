1cf7cf7185fc74ea29f3ae5640ae35c06f281c64
sumit Rana <sumit2628r@gmail.com>
2023-03-08 23:55:41 +0530

    leetcode 75 find pivot updated

--- Leet Code 75/src/level1/prefixSum/Find_Pivot_Index.java
+++ Leet Code 75/src/level1/prefixSum/Find_Pivot_Index.java
@@ -2,9 +2,16 @@ package level1.prefixSum;
 
 public class Find_Pivot_Index {
     public int pivotIndex(int[] nums) {
-        int totalSum =0;
-        for(int i=0; i < nums.length; i++){
-                totalSum +=nums[i];
+        int sum =0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+        int lsum =0, rsum =sum;
+        for (int i = 0; i < nums.length; i++) {
+            rsum -= nums[i];
+
+            if (lsum == rsum) return i;
+            lsum+=nums[i];
         }
 
         return -1;
